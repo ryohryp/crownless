00f3544609fbc735bb444d4453622837e3fd5876
ryohryp <45325933+ryohryp@users.noreply.github.com>
2026-08-30 22:06:18 +0900

    test: assert atlas action loader dependencies

--- test/discovery-actions-atlas-loader.test.js
+++ test/discovery-actions-atlas-loader.test.js
@@ -6,12 +6,9 @@ const path = require("node:path");
 const runtimeSource = fs.readFileSync(path.join(__dirname, "../src/app-runtime-state.js"), "utf8");
 
 test("atlas action model and presentation load after lore presentation", () => {
-  const lorePresentationIndex = runtimeSource.indexOf('lorePresentation.src = "src/world-atlas-lore-presentation.js"');
-  const actionsIndex = runtimeSource.indexOf('actions.src = "src/discovery-actions.js"');
-  const actionsPresentationIndex = runtimeSource.indexOf('presentation.src = "src/world-atlas-actions-presentation.js"');
-  assert.ok(lorePresentationIndex >= 0);
-  assert.ok(actionsIndex > lorePresentationIndex);
-  assert.ok(actionsPresentationIndex >= 0);
+  assert.match(runtimeSource, /lorePresentation\.src = "src\/world-atlas-lore-presentation\.js"/);
+  assert.match(runtimeSource, /actions\.src = "src\/discovery-actions\.js"/);
+  assert.match(runtimeSource, /presentation\.src = "src\/world-atlas-actions-presentation\.js"/);
   assert.match(runtimeSource, /lorePresentation\.onload = loadActionsDomain/);
   assert.match(runtimeSource, /actions\.onload = loadActionsPresentation/);
 });
